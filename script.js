215d68aaa4be35f78d6dcbc2d0c39024d1a59009
Alana <alana.feed@gmail.com>
2020-11-10 14:37:55 -0600

    fix on click behavior of nav headings when wider than 800px

--- script.js
+++ script.js
@@ -11,26 +11,62 @@ const videoLinks = document.getElementsByClassName('video-links')[0]
 const otherButton = document.getElementsByClassName('other-button')[0]
 const otherLinks = document.getElementsByClassName('other-links')[0]
 
+function getFileName() {
+    var url = document.location.pathname;
+    url = url.substring(url.lastIndexOf("/") + 1, url.length);
+    return url;
+}
+
 menuButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 wordButton.addEventListener('click', () => {
-    wordLinks.classList.toggle('active')
-    wordButton.classList.toggle('active')
+    if ($(window).width() < 800) {
+        wordLinks.classList.toggle('active')
+        wordButton.classList.toggle('active')
+    } else if (getFileName() == "index.html") {
+        window.location.href = 'word/word.html'
+    } else {
+        window.location.href = '../word/word.html'
+    }
 })
 pptButton.addEventListener('click', () => {
-    pptLinks.classList.toggle('active')
-    pptButton.classList.toggle('active')
+    if ($(window).width() < 800) {
+        pptLinks.classList.toggle('active')
+        pptButton.classList.toggle('active')
+    } else if (getFileName() == "index.html") {
+        window.location.href = 'ppt/ppt.html'
+    } else {
+        window.location.href = '../ppt/ppt.html'
+    }
 })
 pdfButton.addEventListener('click', () => {
-    pdfLinks.classList.toggle('active')
-    pdfButton.classList.toggle('active')
+    if ($(window).width() < 800) {
+        pdfLinks.classList.toggle('active')
+        pdfButton.classList.toggle('active')
+    } else if (getFileName() == "index.html") {
+        window.location.href = 'pdf/pdf.html'
+    } else {
+        window.location.href = '../pdf/pdf.html'
+    }
 })
 videoButton.addEventListener('click', () => {
-    videoLinks.classList.toggle('active')
-    videoButton.classList.toggle('active')
+    if ($(window).width() < 800) {
+        videoLinks.classList.toggle('active')
+        videoButton.classList.toggle('active')
+    } else if (getFileName() == "index.html") {
+        window.location.href = 'video/video.html'
+    } else {
+        window.location.href = '../video/video.html'
+    }
 })
 otherButton.addEventListener('click', () => {
-    otherLinks.classList.toggle('active')
-    otherButton.classList.toggle('active')
+    if ($(window).width() < 800) {
+        otherLinks.classList.toggle('active')
+        otherButton.classList.toggle('active')
+    } else if (getFileName() == "index.html") {
+        window.location.href = 'other/other.html'
+    } else {
+        window.location.href = '../other/other.html'
+    }
 })
